ba8bea9ccaea1dff7a14019cc1d4bbc66bad8dfd
Davo <davo@email.com>
2026-01-13 16:20:45 -0400

    update sistema

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -8,8 +8,8 @@ export type UserRole = 'ADMIN' | 'SUPERVISOR' | 'OPERADOR';
 export class AuthService {
 
   private readonly ADMIN_USER = {
-    nome: 'Davison',
-    email: 'davison@sistema.com',
+    nome: 'admin',
+    email: 'admin@sistema.com',
     password: '@123456',
     role: 'ADMIN' as UserRole,
   };
